deb6af61434bc2be55aadbf945c0d83b1d4de4d3
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-03-26 19:40:52 -0700

    Criando CampoTexto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto/Index'
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto />
     </div>
   );
 }
