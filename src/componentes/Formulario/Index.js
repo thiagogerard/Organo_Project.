0105cf73c3494d0668a949b2a8fa39d551aabf07
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-04-03 21:32:44 -0700

    Criando lista dos times

--- src/componentes/Formulario/Index.js
+++ src/componentes/Formulario/Index.js
@@ -1,7 +1,18 @@
 import './Formulario.css'
-import CampoTexto from '../CampoTexto/Index'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className="formulario">
             <form>
@@ -9,6 +20,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome"/>
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
