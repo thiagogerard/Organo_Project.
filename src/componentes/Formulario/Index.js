4cd9dd2d6543802af0fd9f0b0e35e868b79bfe41
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-04-16 10:53:07 -0700

    cadastrando colaboradores

--- src/componentes/Formulario/Index.js
+++ src/componentes/Formulario/Index.js
@@ -4,7 +4,7 @@ import ListaSuspensa from '../ListaSuspensa'
 import Botao from '../Botao'
 import { useState } from 'react'
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -23,6 +23,12 @@ const Formulario = () => {
     const aoSalvar = (event) => {
         event.preventDefault();
         console.log('Form foi submetido => ', nome, cargo, imagem, time)
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
 
     return (
